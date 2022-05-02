2d396ed19e1df6d32237511e4cd648c7d2acb30b
Rodney Solomon <rodneysolomonjr@gmail.com>
2022-05-02 16:22:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function testThumb(){
+  for(let i =0; i < 5; i++){
+     console.log(i);
+  }   
+}
+
+testThumb();
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits[3].toString());
+
