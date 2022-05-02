0b65d9ee602d2be43de51a97d7e85b0a3ea93128
Rodney Solomon <rodneysolomonjr@gmail.com>
2022-05-02 16:28:42 +0000

    v1 - refactored ES6 findNemo array - 2022/05/02

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
-function testThumb(){
-  for(let i =0; i < 5; i++){
-     console.log(i);
-  }   
+// function testThumb(){
+//   for(let i =0; i < 5; i++){
+//      console.log(i);
+//   }   
+// }
+
+// testThumb();
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits[3].toString());
+
+const findNemo = array => {
+  array.forEach(fish => {
+    if(fish === 'nemo' {
+      console.log('Found Nemo!')
+    })
+  })
 }
-
-testThumb();
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(fruits[3].toString());
-
